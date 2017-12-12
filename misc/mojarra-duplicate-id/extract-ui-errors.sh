65cadc34018e0f93896be1bc059d8570dbf048f7
Stuart Stephen <stuart.stephen@gmail.com>
2017-12-12 15:55:16 +0000

    Update extract-ui-errors.sh
    Handle additional error formats.

--- misc/mojarra-duplicate-id/extract-ui-errors.sh
+++ misc/mojarra-duplicate-id/extract-ui-errors.sh
@@ -2,4 +2,4 @@
 
 mkdir extract-ui-errors
 rm -rf extract-ui-errors/*
-awk 'NR == 1 {writing=0; count=1}/^SEVERE: JSF1007: Duplicate component ID/{flag=1} writing || flag {print > "errors#"(count)".txt"; writing=1}/^<\/html>/{flag=0; writing=0; count+=1}flag' $@ > nul
+awk 'NR == 1 {writing=0; count=1} $0 ~ /JSF1007: Duplicate component ID/{flag=1} writing || flag {print > "errors#"(count)".txt"; writing=1}/^<\/html>/{flag=0; writing=0; count+=1}flag' $@ > nul
